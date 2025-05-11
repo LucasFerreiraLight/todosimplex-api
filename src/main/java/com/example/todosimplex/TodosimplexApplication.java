7b5a4ca2f6483ec6318fcbdc64c56cbe332ab38a
LucasFerreiraLight <lucasferreiradaluz20@gmail.com>
2025-05-10 22:10:54 -0300

    Project configurations

---
+++ src/main/java/com/example/todosimplex/TodosimplexApplication.java
@@ -0,0 +1,13 @@
+package com.example.todosimplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosimplexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todosimplex/TodosimplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todosimplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
