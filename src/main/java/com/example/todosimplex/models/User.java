1313fcba31c9765c0e7e9a5732a8674db995cf25
LucasFerreiraLight <lucasferreiradaluz20@gmail.com>
2025-05-12 22:40:10 -0300

    Create User model

---
+++ src/main/java/com/example/todosimplex/models/User.java
@@ -0,0 +1,67 @@
+package com.example.todosimplex.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+
+@Entity
+@Table(name = User.Table_Name)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    public interface CreateUser{}
+    public interface UpdateUser{}
+
+    public static final String Table_Name = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+
+    @Column(name = "username", length = 70, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotBlank(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 8, max=70)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class} , min = 8, max=60)
+    private String password;
+
+    //private List<Task> tasks = newArraylist<Task>();
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        User user = (User) obj;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+}
